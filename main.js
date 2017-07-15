f734c1a6bc5f9514fb90a52337df1a8aec2543a1
Brandyn Sullins <woofenwauzer@gmail.com>
2017-07-15 16:04:27 -0400

    took out eval and rewrote the math

--- main.js
+++ main.js
@@ -2,44 +2,64 @@ var numero = document.querySelectorAll('.numbah')
 var operate = document.querySelectorAll('.operator')
 var viewer = document.querySelector('.view')
 var clearout = document.querySelector('.Clear')
-var decimal = document.querySelector('.Decimal')
+// var decimal = document.querySelector('.decimal')
 var equals = document.querySelector('.Equals')
 
+let num1 = ''
+let num2 = ''
+let sign = ''
 let valueHolder = ''
-
-for (let i = 0; i < numero.length; i++) {
-  let current = numero[i]
-  current.addEventListener('click', () => {
-    let value = current.innerText
-    valueHolder += value
-    viewer.textContent = valueHolder
-  })
-}
-
-for (let i = 0; i < operate.length; i++) {
-  let current = operate[i]
-  current.addEventListener('click', () => {
-    let value = current.innerText
-    if (value === 'x') {
-      valueHolder += '*'
-    } else {
-      valueHolder += value
-    }
-    viewer.textContent = valueHolder
-  })
-}
+let answer = ''
 
 clearout.addEventListener('click', () => {
   valueHolder = ''
   viewer.textContent = valueHolder
 })
 
-decimal.addEventListener('click', () => {
-  valueHolder += decimal.innerText
-  viewer.textContent = valueHolder
-})
+// decimal.addEventListener('click', () => {
+//   let current = decimal.innerText
+//   valueHolder += current
+//   viewer.textContent = valueHolder
+// })
+
+for (let i = 0; i < numero.length; i++) {
+  numero[i].addEventListener('click', () => {
+    let current = numero[i].innerText
+    valueHolder += current
+    viewer.textContent = valueHolder
+    if (sign) {
+      num2 += current
+      console.log(num2)
+    } else if (current === '+' || current === '-' || current === 'x' || current === '/') {
+      sign = current
+    } else {
+      num1 += current
+      console.log(num1)
+    }
+  })
+}
 
 equals.addEventListener('click', () => {
-  let calced = eval(valueHolder).toString()
-  viewer.textContent = calced
+  if (sign === '+') {
+    answer = Number(num1) + Number(num2)
+    console.log(sign)
+  }
+  if (sign === '-') {
+    answer = Number(num1) - Number(num2)
+    console.log(sign)
+  }
+  if (sign === 'x') {
+    answer = Number(num1) * Number(num2)
+    console.log(sign)
+  }
+  if (sign === '/') {
+    answer = Number(num1) / Number(num2)
+    console.log(sign)
+  }
+  answer = `${answer}`
+  viewer.textContent = answer
+
+  num1 = ''
+  num2 = ''
+  sign = ''
 })
